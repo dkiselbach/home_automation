0f3165d19f63f2ee9399c8b9990e7c9150e9618e
dkiselbach <dylankiselbach@gmail.com>
2022-06-27 19:00:22 -0500

    camel_case

--- graphql/resolvers/homes.js
+++ graphql/resolvers/homes.js
@@ -1,11 +1,11 @@
 const Home = require('../../models/home');
 
 const homes = async () => {
-  return Home.query().orderBy('createdAt', 'desc');
+  return Home.query().orderBy('created_at', 'desc');
 };
 
 const homeUsers = async (home_id) => {
-  return Home.relatedQuery('users').for(home_id).orderBy('createdAt', 'desc');
+  return Home.relatedQuery('users').for(home_id).orderBy('created_at', 'desc');
 };
 
 module.exports = { homes, homeUsers };
--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,11 +1,11 @@
 const User = require('../../models/user');
 
 const users = async () => {
-  return User.query().orderBy('createdAt', 'desc');
+  return User.query().orderBy('created_at', 'desc');
 };
 
 const userHomes = async (user_id) => {
-  return User.relatedQuery('homes').for(user_id).orderBy('createdAt', 'desc');
+  return User.relatedQuery('homes').for(user_id).orderBy('created_at', 'desc');
 };
 
 module.exports = { users, userHomes };
